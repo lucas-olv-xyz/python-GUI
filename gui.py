7fa2de641672bcde4c0b465183b1b74a1d8b5011
wannacry3030 <wannacry.bat@proton.me>
2022-12-18 17:18:14 -0300

    updated with url

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+import webbrowser
 
 # Create the main window
 window = tk.Tk()
@@ -7,18 +7,19 @@ window.title("Minha interface")
 window.geometry("900x600")
 window.configure(bg='black')
 
-def say_hello():
-    print("Hello,world!")
+def open_url():
+    # Open the URL in the user's default web browser
+    webbrowser.open("https://www.youtube.com/watch?v=OzBPyATdG-c&list=PLnh-TZzbBdaAMOm7sexgpBr01u3uFnjmA&index=4")
     
 # Create a label and a button
 label = tk.Label(window, text="Bem vindo..!", bg='black')
 label.configure(fg='red', font=('Inter', 20))
 
-button = tk.Button(window, text="Entrar no abismo!", command=say_hello)
-button.configure(fg='red', bg='black', font=('Inter', 12))
+button = tk.Button(window, text="????????????", command=open_url)
+button.configure(fg='pink', bg='black', font=('Inter', 12))
 button.pack(side='bottom')
 
-image = tk.PhotoImage(file='outrun.gif')
+image = tk.PhotoImage(file='dream.gif')
 
 label.pack(side='top', fill='both', expand=True)
 label.configure(image=image)
